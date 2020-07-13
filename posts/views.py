8b284a4a0399bdc9c3cb0e24491c6b238f608f94
lhs961021 <lhs961021@naver.com>
2020-07-13 13:59:39 +0900

    20200713homework

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post 
 
-# Create your views here.
+def new(request):
+    return render(request, 'posts/new.html')
+
+
+def create(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        Post.objects.create(title=title, content=content)
+    return redirect('posts:main')
+
+def main(request):
+    posts = Post.objects.all()
+    return render(request, 'posts/main.html', {'posts':posts})
+
+def show(request, id):
+    post = Post.objects.get(pk=id)
+    A = post.view_count
+    A = A + 1
+    post.view_count = A
+    post.save() 
+    return render(request, 'posts/show.html', {'post': post})
