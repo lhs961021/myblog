eb8ccb99fb4b9217f3af2a7c82aeb64527407d5b
lhs961021 <lhs961021@naver.com>
2020-07-15 20:24:21 +0900

    20200722homework

--- posts/urls.py
+++ posts/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('create/',create, name="create"),
     path('', main, name="main"),
     path('show/<int:id>', show, name="show"),
+    path('update/<int:id>', update, name="update"),
+    path('delete/<int:id>', delete, name="delete"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post 
 
 def new(request):
@@ -9,7 +9,8 @@ def create(request):
     if request.method == "POST":
         title = request.POST.get('title')
         content = request.POST.get('content')
-        Post.objects.create(title=title, content=content)
+        image = request.FILES.get('image')
+        Post.objects.create(title=title, content=content, image=image)
     return redirect('posts:main')
 
 def main(request):
@@ -23,3 +24,18 @@ def show(request, id):
     post.view_count = A
     post.save() 
     return render(request, 'posts/show.html', {'post': post})
+
+def update(request,id):
+    post = get_object_or_404(Post,pk=id)
+    if request.method == "POST":
+        post.title = request.POST['title']
+        post.content = request.POST['content']
+        post.image = request.FILES.get('image')
+        post.save()
+        return redirect('posts:show',post.id)
+    return render(request,'posts/update.html',{"post":post})
+
+def delete(request,id):
+    post = get_object_or_404(Post, pk=id)
+    post.delete()
+    return redirect("posts:main")
